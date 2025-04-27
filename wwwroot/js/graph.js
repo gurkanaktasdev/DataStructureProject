b27c79aa963cd13e0a1f751c3393b9133467c9a2
Gürkan Aktaş <146657743+gurkanaktasdev@users.noreply.github.com>
2025-04-27 13:04:54 +0300

    Node ve Edge silme

--- wwwroot/js/graph.js
+++ wwwroot/js/graph.js
@@ -1,4 +1,4 @@
-﻿var cy = cytoscape({
+﻿var cy = cytoscape({    // cytoscape üzerinden oluşturduğumuz düğüm ve kenarları tutmamıza yarar.
     container: document.getElementById('cy'),
     elements: [],
     style: [
@@ -31,6 +31,70 @@ cy.on('cxttap', function(event) {
     console.log('Tür:', element.group);
 });*/
 });
+// İki tıklama sonunda  edge oluşturma
+let sourceNode = null;
+
+cy.on('tap', function(event) {
+    if (event.target.group && event.target.group() === 'nodes') {
+        if (sourceNode == null) {
+            // İlk tıklanan düğüm source olacak
+            sourceNode = event.target;
+            sourceNode.style('background-color', '#FF4136'); // seçildiğini belli et
+        } else {
+            // İkinci düğüm target olacak
+            var targetNode = event.target;
+            if (sourceNode.id() !== targetNode.id()) {      // burada arka arkaya aynı düğüm seçilmesin diye konumuş bir koşul
+                var weight = prompt("Edge ağırlığı (sayı gir):", "1");
+                if (weight != null) {
+                    cy.add({
+                        group: 'edges',
+                        data: { 
+                            source: sourceNode.id(), 
+                            target: targetNode.id(),
+                            weight: parseInt(weight)
+                        }
+                    });
+                }
+            }
+            // Renkleri sıfırla
+            sourceNode.style('background-color', '#0074D9');
+            sourceNode = null;
+        }
+    }
+});
+
+
+// Node veya Edge Silme  -> klavyeden gelen d nodeları, e ise kenarları silmek için
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'n') {  // 'n' tuşuna basıldığında Node ekleme moduna geç
+        mode = 'node';
+        var deleteNode = prompt("Silmek istediğiniz Node");
+        var nodeExists = cy.elements('#' + deleteNode).length > 0;
+        if(nodeExists){
+            var nodeToDelete = cy.getElementById(deleteNode);  // Node'u al
+            nodeToDelete.remove();
+        }else{
+            console.log("geçersiz Id girildi.")
+        }
+    }
+    if (event.key === 'e') {
+        var sourceNodeId = prompt("Silmek istediğiniz Edge'in Source Node ID'sini girin:");
+        var targetNodeId = prompt("Silmek istediğiniz Edge'in Target Node ID'sini girin:");
+
+        var edgeExists = cy.edges(`[source = '${sourceNodeId}'][target = '${targetNodeId}']`).length > 0;
+
+        if (edgeExists) {
+            var edgeToDelete = cy.edges(`[source = '${sourceNodeId}'][target = '${targetNodeId}']`);
+            edgeToDelete.remove();  // Edge'i sil
+            console.log("Edge başarıyla silindi.");
+        } else {
+            console.log("Geçersiz Source veya Target ID'si.");
+        }
+    }
+});
+
+
+
 // backande kısmını dinamik oluşturduğumuz graph veriyapısını düzenlememize olanak sağlar.
 function exportGraphData() {
     var nodes = cy.nodes().map(node => ({
@@ -78,37 +142,5 @@ function sendGraphToServer() {
 }
 
 
-
-// İki tıklama ile edge oluşturma
-let sourceNode = null;
-
-cy.on('tap', function(event) {
-    if (event.target.group && event.target.group() === 'nodes') {
-        if (sourceNode == null) {
-            // İlk tıklanan düğüm source olacak
-            sourceNode = event.target;
-            sourceNode.style('background-color', '#FF4136'); // seçildiğini belli et
-        } else {
-            // İkinci düğüm target olacak
-            var targetNode = event.target;
-            if (sourceNode.id() !== targetNode.id()) {      // burada arka arkaya aynı düğüm seçilmesin diye konumuş bir koşul
-                var weight = prompt("Edge ağırlığı (sayı gir):", "1");
-                if (weight != null) {
-                    cy.add({
-                        group: 'edges',
-                        data: { 
-                            source: sourceNode.id(), 
-                            target: targetNode.id(),
-                            weight: parseInt(weight)
-                        }
-                    });
-                }
-            }
-            // Renkleri sıfırla
-            sourceNode.style('background-color', '#0074D9');
-            sourceNode = null;
-        }
-    }
-});
 
 
