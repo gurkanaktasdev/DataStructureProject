462de301b1966faf5b820ed19ab71ea1d392cb1c
mustafasa1t <mustafasaitcfc60@gmail.com>
2025-05-12 16:56:20 +0300

    kruskal algoritması için uygun formatta yollama

--- Controllers/APIController.cs
+++ Controllers/APIController.cs
@@ -26,39 +26,68 @@ public class GraphController : ControllerBase
         {
             graph.AddEdge(edge.Source!, edge.Target!, edge.Weight);
 
-             if (!graphData.IsDirected)
+            if (!graphData.IsDirected)
             {
                 graph.AddEdge(edge.Target!, edge.Source!, edge.Weight);
             }
         }
         graph.PrintGraph();
         Console.WriteLine("----------------");
-         Dictionary<string, int> deneme = null;
-         if (graphData.forDijsktra != null)
+        Dictionary<string, int> dijsktradeneme = null;
+        if (graphData.forDijsktra != null)
         {
-            deneme = graph.Dijkstra(graphData.forDijsktra);
-            foreach (var d in deneme)
+            dijsktradeneme = graph.Dijkstra(graphData.forDijsktra);
+            foreach (var d in dijsktradeneme)
             {
                 Console.WriteLine($"{d.Key} ->" + d.Value);
 
             }
         }
-        Console.WriteLine($"{graphData.isPrim} prim  degeri"); // kontrol için yazdırıyorum
-        if(graphData.isPrim)
+        Console.WriteLine("------------------");
+        List<Object> primResult = null;
+        if (graphData.isPrim != null)
         {
-            var primDeneme = graph.PrimMST("a");
-            foreach(var a in primDeneme)
+            var primDeneme = graph.PrimMST(graphData.isPrim);
+            foreach (var a in primDeneme)
             {
                 Console.WriteLine($"{a.from} - {a.to} -> {a.weight}");
             }
+            primResult = primDeneme.Select(edge => (object)new   // tuple nesnesini otmatik json a çeviremediği için böyle bir yol izledim
+            {
+                from = edge.Item1,
+                to = edge.Item2,
+                weight = edge.Item3
+            }).ToList();
         }
 
+        Console.WriteLine($"Kruskal sonucu:{graphData.isKruskal}");
+        List<object> kruskalResult = null;
+        if (graphData.isKruskal == true)
+        {
+            var kruskalDeneme = graph.Kruskal();
+            foreach (var (source, edge) in kruskalDeneme)
+            {
+                Console.WriteLine($"{source} -> {edge.TargetNode.Id} (Weight: {edge.Weight})");
+            }
+            kruskalResult = kruskalDeneme.Select(edge => (object)new
+            {
+                from = edge.SourceId,
+                to = edge.Edge.TargetNode.Id,
+                weight = edge.Edge.Weight
+            }).ToList();
 
-        return Ok(new {
+
+        }
+        return Ok(new
+        {
             message = "Graf başarıyla alındı",
             nodeCount = NodeSayisi,
             edgeCount = KenarSayisi,
-            dijkstraSonuc = deneme
+            dijkstraSonuc = dijsktradeneme,
+            primSonuc = primResult,
+            kruskalSonuc = kruskalResult
+
         });
     }
 }
+
